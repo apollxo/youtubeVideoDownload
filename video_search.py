51dca60e4c2f3a1bdc613e51bf93ca5abc6d7b5b
umidevop <umidevop@sysXcall>
2023-07-01 19:32:48 +0500

    added

---
+++ video_search.py
@@ -0,0 +1,12 @@
+import urllib.request
+import re
+
+ask = input('Enter youtube video name: \n')
+newStr = ask.replace(' ', '+')
+html = urllib.request.urlopen(f'https://www.youtube.com/results?search_query={newStr}')
+video_ids = re.findall(r'watch\?v=(\S{11})', html.read().decode())
+
+ids = list(set(video_ids))
+#print(ids)
+for i in range(len(ids)):
+	print('https://www.youtube.com/watch?v=' + ids[i]) 
