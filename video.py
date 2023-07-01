de30f45cd601ccbd079cf3be0f09d7d2f02a6c47
umidevop <umidevop@sysXcall>
2023-07-01 19:34:00 +0500

    added

---
+++ video.py
@@ -0,0 +1,44 @@
+from pytube import YouTube
+import subprocess
+import os
+
+video = input('Enter Youtube Share Link\n')
+
+yt = YouTube(video)
+
+yt.title = 'input'
+
+yt.streams.filter(adaptive=True)
+
+video_stream = yt.streams.get_by_itag(18)
+audio_stream = yt.streams.get_by_itag(250)
+print('downloading video...')
+video_stream.download()
+print('downloading audio...')
+audio_stream.download()
+print('Merging audio and video...')
+
+input_video = 'input.mp4'
+input_audio = 'input.webm'
+output = 'output.mp4'
+
+command = [
+	'ffmpeg',
+	'-i', input_video,
+	'-i', input_audio,
+	'-c:V', 'copy',
+	'-c:a', 'copy',
+	output
+]
+
+with open("/dev/null", "w") as black_hole:
+    subprocess.run(command, stdout=black_hole, stderr=black_hole, check=True)
+
+print('deleting files...')
+os.remove(input_video)
+os.remove(input_audio)
+
+print('Finished')
+
+
+
