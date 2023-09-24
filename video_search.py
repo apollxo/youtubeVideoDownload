cba8cbf62f2913372c10b2c0a72181aa8cc5800b
Apollo <112485583+apollxo@users.noreply.github.com>
2023-09-24 20:47:52 +0500

    Update video_search.py

--- video_search.py
+++ video_search.py
@@ -11,18 +11,3 @@ ids = list(set(video_ids))
 for i in range(len(ids)):
 	print('https://www.youtube.com/watch?v=' + ids[i]) 
 
-# from moviepy.video.io.VideoFileClip import VideoFileClip
-
-# # Load the video file
-# video = VideoFileClip('input.mp4')
-
-# # Set the start and end times of the clip you want to extract (in seconds)
-# start_time = 5
-# end_time = 15
-
-# # Use the subclip method to extract the clip
-# clip = video.subclip(start_time, end_time)
-
-# # Save the extracted clip to a new video file
-# clip.write_videofile('output.mp4')
-# print('proccess is done!!!')
